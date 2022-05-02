44de881c6537bef61598445a2b3e8d7bb623617a
alexandrexpj <alexandrexpjr@hotmail.com>
2022-05-02 10:26:42 -0300

    feat: Desenvolve métodos `subtraction()` e `sum()`

---
+++ src/calculator.ts
@@ -0,0 +1,29 @@
+export default class Calculator {
+  private static _memorizedNumber: number;
+
+  public static get memorizedNumber(): number {
+    return Calculator._memorizedNumber;
+  }
+
+  public static set memorizedNumber(value: number) {
+    Calculator._memorizedNumber = value;
+  }
+
+  public static sum(a: number, b: number = Calculator.memorizedNumber) {
+    const result = this.normalize(a + b);
+
+    this.memorizedNumber = result;
+    return result;
+  }
+  
+  public static subtraction(a: number, b: number = Calculator.memorizedNumber) {
+    const result = this.normalize(b - a);
+
+    this.memorizedNumber = result;
+    return result;
+  }
+
+  private static normalize(n: number) {
+    return parseFloat(n.toPrecision(12));
+  }
+}
